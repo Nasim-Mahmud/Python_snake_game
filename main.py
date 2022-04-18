97ae6edcb1bfdc86425bb7dcd3c68a231687d847
Nasim Mamud Mishu <57942968+Nasim-Mahmud@users.noreply.github.com>
2022-04-19 00:04:41 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake game")
 screen.tracer(0)
-# TODO: there are various methods to make snakes
-# x = 0
-# for n in range(0, 3):
-#     tim = Turtle("square")
-#     tim.penup()
-#     tim.color("white")
-#     tim.setx(x)
-#     x += 20
+screen.title("Sanaka Gamass")
 
-starting_pos = [(0, 0), (-20, 0), (-40, 0)]
-ggturtles = []
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-for position in starting_pos:
-    new_turtle = Turtle("square")
-    new_turtle.penup()
-    new_turtle.color("white")
-    new_turtle.goto(position)
-    ggturtles.append(new_turtle)
-
-# screen.update()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(.1)
+    time.sleep(0.1)
+    snake.move()
 
-    for tur_num in range(len(ggturtles) - 1, 0, -1):
-        new_x = ggturtles[tur_num - 1].xcor()
-        new_y = ggturtles[tur_num - 1].ycor()
-        ggturtles[tur_num].goto(new_x, new_y)
-    ggturtles[0].forward(20)
-    ggturtles[0].left(90)
+#     Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
 
+#     Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+
+#         Detect collision with tail
+    for tur in snake.ggturtles[1:]:
+        if snake.head.distance(tur) < 5:
+            game_is_on = False
+            scoreboard.game_over()
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,53 @@
+from turtle import Turtle, Screen
+
+turtle_pos = [(0, 0), (-20, 0), (-40, 0)]
+MOVE = 20
+screen = Screen()
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.ggturtles = []
+        self.create_snake()
+        self.head = self.ggturtles[0]
+
+    def create_snake(self):
+        for pos in turtle_pos:
+            self.add_turtle(pos)
+
+    def add_turtle(self, pos):
+        tim = Turtle("square")
+        tim.color("white")
+        tim.penup()
+        tim.goto(pos)
+        self.ggturtles.append(tim)
+
+    def extend(self):
+        self.add_turtle(self.ggturtles[-1].position())
+
+    def move(self):
+        for n in range(len(self.ggturtles) - 1, 0, -1):
+            x_pos = self.ggturtles[n - 1].xcor()
+            y_pos = self.ggturtles[n - 1].ycor()
+            self.ggturtles[n].goto(x_pos, y_pos)
+        self.head.forward(MOVE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
