409df69cb95f815ca1d097d1715bac1809070917
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-07 20:47:10 +0600

    Storing high score in files.

--- scoreboard.py
+++ scoreboard.py
@@ -24,8 +24,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             with open("data.txt", mode="w") as data:
-                data.write(f"{self.score}")
-                self.high_score = int(data.read())
+                data.write(f"{self.high_score}")
             # self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
